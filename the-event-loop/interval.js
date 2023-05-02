ab4eea8704b078a34df3e4479c5af0a3d827f693
Marianne Cuevas <mariannecuevas97@gmail.com>
2023-05-02 03:47:36 +0000

    Completed the-event-loop exercise.

---
+++ the-event-loop/interval.js
@@ -0,0 +1,11 @@
+let count = 3;
+
+const timer = setInterval(() => {
+  console.log(count);
+  count--;
+
+  if (count === 0) {
+    console.log('Blast off!');
+    clearInterval(timer);
+  }
+}, 1000);
